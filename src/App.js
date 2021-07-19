f4c4c3d7278d24bbbd41e2a016fef9af31ef0455
akhtech94 <akhtech94@gmail.com>
2021-07-19 07:17:44 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import LoginOrSignup from './components/LoginOrSignup'
+
+function App() {
+  return (
+    <div className="App">
+      <LoginOrSignup />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/LoginOrSignup.js
@@ -0,0 +1,7 @@
+export default function loginOrSignup() {
+    return (
+        <div>
+            <h1>loginOrSignup page coming soon!!!</h1>
+        </div>
+    );
+}
